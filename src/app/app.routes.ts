84000b1b332b87fb68a7c25896fc2e27890bf57d
Salaheddine Belhaska <salaheddine.belhaska@sio-carriat.com>
2026-05-11 15:36:09 +0200

    ajout du lien du nouveau composant utlisateurs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ComposantCatalogue } from './composant-categorie/composant-categorie'
 import { ComposantConnexion } from './composant-connexion/composant-connexion'
 import { ComposantContact } from './composant-contact/composant-contact'
 import { ComposantAdmin } from './composant-admin/composant-admin'
+import { ComposantUtlisateurs } from './composant-utlisateurs/composant-utlisateurs'
 
 export const routes: Routes = [
   { path: '', component: Accueil },
@@ -11,4 +12,5 @@ export const routes: Routes = [
   { path: 'connexion', component: ComposantConnexion },
   { path: 'contact', component: ComposantContact },
   { path: 'admin', component: ComposantAdmin },
+  {path:'utlisateurs' , component : ComposantUtlisateurs}
 ]
