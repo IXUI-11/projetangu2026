311200cd402543233236c32d2e53c0c0c4343378
Salaheddine Belhaska <salaheddine.belhaska@sio-carriat.com>
2026-05-11 15:48:15 +0200

    le line de l'api pour une nouvelle en local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://172.20.129.1/API', // ← remplace par l'URL de prod
+  // apiUrl: 'https://172.20.129.1/API', // ← remplace par l'URL de prod
+  apiUrl : 'https://localhost:7051',
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://172.20.129.2',
+  // apiUrl: 'http://172.20.129.2',
+apiUrl : 'https://localhost:7051',
 }
+
