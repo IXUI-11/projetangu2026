821d0b2f8f25fc42f84f0b3a9577a98375e55ba7
IXUI-11 <lazalasa1990@gmail.com>
2026-05-28 18:02:40 +0200

    ajout des champs manquants dans le model de materiel

--- src/models/materiel.model.ts
+++ src/models/materiel.model.ts
@@ -1,7 +1,9 @@
 export class Materiel {
   id!: number
-  nom!: string
+  nomMateriel!: string
   description!: string
   image!: string
   prix!: number
+  idCategorie!: number
+  valeur!: string
 }
