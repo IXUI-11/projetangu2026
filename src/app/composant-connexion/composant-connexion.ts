fcae37671bb1348a8173dc7aac68e48fc2d09c21
IXUI-11 <lazalasa1990@gmail.com>
2026-05-28 18:19:58 +0200

    feat(connexion): gestion connexion et inscription avec JWT
    - Ajout sauvegarde token JWT après connexion
    - Ajout détection rôle Admin via boucle for sur roles[]
    - Redirection /admin si Admin, /utlisateurs si User
    - Ajout sauvegarde rôle dans localStorage
    - Ajout champs inscription : ville, rue, codePostal, numerodetelephone

--- src/app/composant-connexion/composant-connexion.ts
+++ src/app/composant-connexion/composant-connexion.ts
@@ -20,7 +20,8 @@ export class ComposantConnexion implements OnInit, OnDestroy {
   numerodetelephone: number = 0
   switchFormulaire: boolean = false
 
-  constructor(private authService: Auth , private router : Router) {}
+
+  constructor(private authService: Auth, private router: Router) { }
 
 
   // ! empêche le scroll de la page quand le composant est affiché
@@ -41,19 +42,44 @@ export class ComposantConnexion implements OnInit, OnDestroy {
   //   })
   // }
 
+
+  /**
+   * Gère le processus d'authentification de l'utilisateur.
+   * * Cette méthode envoie les identifiants (email et mot de passe) au service d'authentification.
+   * En cas de succès :
+   * 1. Sauvegarde le token JWT retourné par l'API.
+   * 2. Parcourt les rôles de l'utilisateur pour vérifier s'il possède les droits d'administrateur.
+   * 3. Sauvegarde le rôle correspondant ('Admin' ou 'User') via le service d'authentification.
+   * 4. Redirige l'utilisateur vers la route appropriée (/admin pour les administrateurs, /utlisateurs pour les clients).
+   * * En cas d'échec (ex: mauvais identifiants), l'erreur est interceptée et affichée dans la console.
+    **/
   seConnecter() {
-  this.authService.login(this.email, this.motDePasse).subscribe({
-    next: (reponseApiJwt: any) => {
-      this.authService.sauvegarderTokenJwt(reponseApiJwt.accessToken)
-      console.log('Connecté !')
-      this.router.navigate(['/']) // ← redirige vers l'accueil
-    },
-    error: (err: any) => console.log(err.error)
-  })
+    this.authService.login(this.email, this.motDePasse).subscribe({
+      next: (reponseApiJwt: any) => {
+        this.authService.sauvegarderTokenJwt(reponseApiJwt.accessToken)
+
+        const roles = reponseApiJwt.user.roles
+        let cEstAdmin = false
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i] === 'Admin') {
+            cEstAdmin = true
+          }
+        }
+
+        if (cEstAdmin) {
+          this.authService.sauvegarderRole('Admin')
+          this.router.navigate(['/admin'])
+        } else {
+          this.authService.sauvegarderRole('User')
+          this.router.navigate(['/utlisateurs'])
+        }
+      },
+      error: (err: any) => console.log(err.error)
+    })
   }
 
-
- sInscrire() {
+  //rassemble les informations saisies dans le formulaire
+  sInscrire() {
     this.authService.register({
       email: this.email,
       password: this.motDePasse,
